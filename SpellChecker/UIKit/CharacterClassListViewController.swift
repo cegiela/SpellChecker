0c561c244a62a818d9d44de054f1118922bd1f79
Mat Cegiela <cegiela@gmail.com>
2022-11-16 09:26:31 +0100

    Organize files

--- SpellChecker/UIKit/CharacterClassListViewController.swift
+++ SpellChecker/UIKit/CharacterClassListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CharacterClassListViewController.swift
 //  SpellChecker
 //
 //  Created by Mat Cegiela on 11/16/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CharacterClassListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
